7740da9642c2302b43af13ff67f1b943cd928f20
maneesha.s <maneesha.s@cubettech.com>
2023-12-01 16:28:18 +0530

    webserver in bun

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+  port: 3000,
+  fetch(res) {
+    const body = figlet.textSync("Hai! Hello!!!!");
+
+    return new Response(body);
+    // return new Response("Hello World");
+  },
+});
+
+console.log(`Listening to port ${server.port}`);
